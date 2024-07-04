1824c683eb63f31ed60c95ffd9d55e5c0dee269c
docobura <docaystar@gmail.com>
2024-07-04 13:02:16 +0200

    change

--- server/models.py
+++ server/models.py
@@ -1,18 +1,61 @@
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import MetaData
+from sqlalchemy.ext.associationproxy import association_proxy
+from sqlalchemy_serializer import SerializerMixin
 
-# contains definitions of tables and associated schema constructs
-metadata = MetaData()
 
-# create the Flask SQLAlchemy extension
+metadata = MetaData(naming_convention={
+    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+})
+
 db = SQLAlchemy(metadata=metadata)
 
-# define a model class by inheriting from db.Model.
 
-
-class Pet(db.Model):
-    __tablename__ = 'pets'
+class Customer(db.Model, SerializerMixin):
+    __tablename__ = 'customers'
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
-    species = db.Column(db.String)
+
+    reviews = db.relationship('Review', back_populates='customer')
+    items = association_proxy('reviews', 'item')
+
+    # SerializerMixin configuration
+    serialize_rules = ('-reviews.customer',)  # Exclude reviews.customer
+
+    def __repr__(self):
+        return f'<Customer {self.id}, {self.name}>'
+
+
+class Item(db.Model, SerializerMixin):
+    __tablename__ = 'items'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    price = db.Column(db.Float)
+
+    reviews = db.relationship('Review', back_populates='item')
+
+    # SerializerMixin configuration
+    serialize_rules = ('-reviews.item',)  # Exclude reviews.item
+
+    def __repr__(self):
+        return f'<Item {self.id}, {self.name}, {self.price}>'
+
+
+class Review(db.Model, SerializerMixin):
+    __tablename__ = 'reviews'
+
+    id = db.Column(db.Integer, primary_key=True)
+    comment = db.Column(db.String)
+    customer_id = db.Column(db.Integer, db.ForeignKey('customers.id'))
+    item_id = db.Column(db.Integer, db.ForeignKey('items.id'))
+
+    customer = db.relationship('Customer', back_populates='reviews')
+    item = db.relationship('Item', back_populates='reviews')
+
+    # SerializerMixin configuration
+    serialize_rules = ('-customer.reviews', '-item.reviews')  # Exclude customer.reviews and item.reviews
+
+    def __repr__(self):
+        return f'<Review {self.id}, Customer {self.customer_id}, Item {self.item_id}, Comment {self.comment}>'
